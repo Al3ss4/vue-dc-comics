18ec98e7a7220d00402c8f64772fdfd6b2d56c21
Mario Rovito <alessandro.r14@gmail.com>
2022-04-06 16:07:09 +0200

    add img and bg with sass

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,20 @@ export default {
 </script>
 
 <style lang="scss">
+$bg-site: #080707;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: $bg-site;
 }
 </style>
