9081d293455deb5274fe00fa57a5e6e92d5ec1f0
Mario Rovito <alessandro.r14@gmail.com>
2022-04-07 17:47:05 +0200

    fix code

--- src/components/Comics.vue
+++ src/components/Comics.vue
@@ -13,29 +13,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-.container{
+.container {
   display: flex;
   flex-direction: column;
   width: calc(100% / 6);
 
-  
-  img{
-   display: block;
-   width: 100%;
-   padding: 20px;
-        &:hover
-        {
-            transform: scale(1.1);
-            cursor: pointer;
-        }
+  img {
+    padding: 20px;
+    object-fit: cover;
+    object-position: 0% 0%;
+    &:hover {
+      transform: scale(1.1);
+      cursor: pointer;
+    }
   }
-  h2{
+  h2 {
     text-transform: uppercase;
     font-size: 0.9rem;
     padding: 0 20px;
-  
   }
 }
-
 </style>
