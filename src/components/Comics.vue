3a1a1fce2d3791ab6cbae04a91ca624e004c1a7d
Mario Rovito <alessandro.r14@gmail.com>
2022-04-07 16:25:15 +0200

    add comics component and link in main.vue

---
+++ src/components/Comics.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img :src="element.thumb" />
+    <h2>{{ element.series }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Comics-series",
+  props: ["element"],
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,38 @@
 <template>
   <main>
-    <main>--&gt; Content goes here &lt;--</main>
+    <div>
+      <div>
+        <Comics
+          v-for="(item, index) in series"
+          :key="index"
+          :element="item"
+        />
+      </div>
+      <a href="#">
+        <h3>LOAD MORE</h3>
+      </a>
+    </div>
   </main>
 </template>
 
 <script>
+import Comics from "@/components/Comics.vue";
+import Series from "@/data/dc-comics.js";
+
 export default {
   name: "MainComponent",
+  components: {
+    Comics,
+  },
+  data() {
+    return {
+      series: Series
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped >
-
 $bg-main: #1c1c1c;
 $color-main: white;
 
@@ -23,6 +44,5 @@ main {
   display: flex;
   align-items: flex-start;
   font-size: 30px;
-  
 }
 </style>
